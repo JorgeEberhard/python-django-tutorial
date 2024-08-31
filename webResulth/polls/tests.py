27215a4207901645b7853a61e61f8dd21d33b720
Jorge Eberhard <jorgem.eberhardc@gmail.com>
2024-08-31 11:58:27 -0300

    Iniciado tutorial de testes

--- webResulth/polls/tests.py
+++ webResulth/polls/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
 # Create your tests here.
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        # Retorna False para perguntas em que a publicação tenha sido feita no futuro
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        # Retorna False para perguntas em que pub_date for mais velha que um dia
+         
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        # Retorna True para perguntas que pub_date for dentro do ultimo dia
+         
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
